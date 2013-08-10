c96553350bcd830d7ef57c79540b94d1339a68d6
Wayne Lovely <wayne.lovely@gmail.com>
2013-08-10 12:12:55 -0400

    initial commit on azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PassengerBlog::Application.config.secret_key_base = '46004bb4dbad9727657155cbb0a25419ba4b0aeacfa3d01367e7a79a73d572d2b716ea980c893cc18aebb081efc9e2a847a05e498ba87469aa736225166ac543'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PassengerBlog::Application.config.session_store :cookie_store, key: '_passenger_blog_session'
